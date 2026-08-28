b76ff1a9b91454710f019ea5a2efed04b2443ee0
Kundan Kumar <kundan2126kumar@gmail.com>
2026-08-28 23:45:48 +0530

    Create MovieRepository.java

---
+++ backend/src/main/java/com/example/moviebooking/repository/MovieRepository.java
@@ -0,0 +1,3 @@
+package com.example.moviebooking.repository;
+import com.example.moviebooking.model.Movie; import org.springframework.data.jpa.repository.JpaRepository;
+public interface MovieRepository extends JpaRepository<Movie,Long>{}
